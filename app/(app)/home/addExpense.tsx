467330c3d5c0bd223c9a31f6e2b979f1ff685323
Maciej Wiacek <maciejwiacek4@gmail.com>
2025-07-27 13:46:58 +0200

    Navigation reset while switching tabs fixed with useEffect

--- app/(app)/home/addExpense.tsx
+++ app/(app)/home/addExpense.tsx
@@ -1,26 +1,24 @@
-import FontAwesome6 from '@expo/vector-icons/FontAwesome6'
+import Heading from '@/components/Heading'
 import { useRouter } from 'expo-router'
 import React from 'react'
-import { Text, TouchableOpacity, View } from 'react-native'
+import { View } from 'react-native'
 
 const AddExpense = () => {
   const router = useRouter()
 
-  const handleBackPress = () => {
+  const handleAddPress = () => {
+    // Logic to handle adding an expense
+
     router.back()
   }
 
   return (
     <View className='flex-1 bg-white pt-20 px-4'>
-      <View className='flex-row items-center justify-between'>
-        <TouchableOpacity className='flex-1' onPress={handleBackPress}>
-          <FontAwesome6 name='chevron-left' size={24} color='#2563eb' />
-        </TouchableOpacity>
-        <Text className='text-center text-xl font-bold'>Dodaj Transakcję</Text>
-        <TouchableOpacity className='flex-1 items-end'>
-          <Text className='text-blue-600 font-bold text-lg'>Dodaj</Text>
-        </TouchableOpacity>
-      </View>
+      <Heading
+        title='Dodaj Transakcję'
+        onPress={handleAddPress}
+        buttonLabel='Dodaj'
+      />
     </View>
   )
 }
